73a7d36b4f8fd5a6c5a1d414d1a80bb487b949ff
Cintia <cintia.dev2024@gmail.com>
2024-11-30 20:05:27 -0300

    Modificação pós correção pelo DevQuest

--- src/JS/index.js
+++ src/JS/index.js
@@ -4,28 +4,47 @@ document.addEventListener("DOMContentLoaded", ()=> {
     const inputsFormulario = document.querySelectorAll(".input");
 
 
-    botaoEnviar.addEventListener('click', () => {
-        inputsFormulario.forEach(input => {
-            if (input.value == "") {
-                input.style.borderColor = "#F52E2E";
-                input.nextElementSibling.style.visibility = 'visible'
-            }
-        })
+    // botaoEnviar.addEventListener('click', (e) => {
+    //     e.preventDefault()
+        
+    //     inputsFormulario.forEach(input => {
+    //         if (input.value == "") {
+    //             input.style.borderColor = "#F52E2E";
+    //             input.nextElementSibling.style.visibility = 'visible'
+    //         }
+    //     })
+    // })
+
+
+    // inputsFormulario.forEach(input => {
+
+    //     input.addEventListener('change', (e) => {
+    //         if (input.value !== "") {
+    //             input.style.borderColor = "#00C22B";
+    //             input.nextElementSibling.style.visibility = 'hidden'
+    //         } else if (input.value == "") {
+    //             input.style.borderColor = "black";
+    //         }
+    //     })
+    // })
+
+        botaoEnviar.addEventListener('click', (e)=>{
+            e.preventDefault()
+        
+    inputsFormulario.forEach((input)=>{
+        console.log(input.value)
+        if(input.value !== ""){
+            input.classList.remove('erro')
+            input.classList.add('valido')
+            input.nextElementSibling.style.visibility = 'hidden'
+        }else{
+            input.classList.remove('valido')
+            input.classList.add('erro')
+            input.nextElementSibling.style.visibility = 'visible'
+        }
     })
 
-
-    inputsFormulario.forEach(input => {
-
-        input.addEventListener('change', (e) => {
-            if (input.value !== "") {
-                input.style.borderColor = "#00C22B";
-                input.nextElementSibling.style.visibility = 'hidden'
-            } else if (input.value == "") {
-                input.style.borderColor = "black";
-            }
-        })
-    })
 })
 
 
-
+})
